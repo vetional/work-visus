3e136982cbb3986e8b5066e829ea05ef97d1161f
Prateek Shrivastava <prateek.shrvstv@gmail.com>
2014-03-09 17:56:50 +0100

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS
+#define COSNTANTS
+
+
+float h=10*SPHSimulation::globalRadius;
+float dt=0.001f;
+float vmax=100;//for dt update
+float etaMax=20;//for dt update
+float alpha=0.5;//bulk viscosity
+float epsilon=0.1f;//for velocity correction
+float roh0=1;
+float c=300000;// in m/sec
+float n=0.5f;
+
+#endif
